9977dea03bdf49f4f323241b225ebf3531639624
DanielSimonsen90 <danielsimonsen90@gmail.com>
2020-09-21 13:29:19 +0200

    Dark theme is cool

--- FolderDetails/FolderDetails/MainWindow.xaml.cs
+++ FolderDetails/FolderDetails/MainWindow.xaml.cs
@@ -20,11 +20,9 @@ namespace FolderDetails
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool ViewDocking = false;
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        private bool ViewDocking = false, 
+            DarkThemeEnabled = false;
+        public MainWindow() => InitializeComponent();
 
         #region MenuItems_Click
         private void OpenFolder_Click(object sender, RoutedEventArgs e)
@@ -32,10 +30,11 @@ namespace FolderDetails
 
         }
 
-        private void ViewDocking_Click(object sender, RoutedEventArgs e)
+        private void ChangeAppearance()
         {
-            if (!ViewDocking)
+            if (ViewDocking)
             {
+                MenuButton.Background = Brushes.White;
                 MenuPanel.Background = Brushes.Red;
                 StatusLinePanel.Background = Brushes.Chocolate;
                 TreeViewContainer.Background = Brushes.Orange;
@@ -49,22 +48,97 @@ namespace FolderDetails
                 lblItemContentLastModifiedTitle.Background = Brushes.LightGray;
                 lblItemContentLastModified.Background = Brushes.Gray;
             }
-            else
+            else if (!DarkThemeEnabled)
             {
+                MenuButton.Background = Brushes.White;
+                MenuButton.Foreground = Brushes.Black;
                 MenuPanel.Background = Brushes.White;
+
+                foreach (var item in MenuButton.ItemContainerGenerator.Items)
+                {
+                    ((MenuItem)item).Background = Brushes.White;
+                    ((MenuItem)item).Foreground = Brushes.Black;
+                }
+
                 StatusLinePanel.Background = Brushes.White;
+                lblStatusLine.Foreground = Brushes.Black;
                 TreeViewContainer.Background = Brushes.White;
+                TreeViewContainer.Foreground = Brushes.Black;
                 ItemContentPanel.Background = Brushes.White;
                 lblItemContentTitle.Background = Brushes.White;
+                lblItemContentTitle.Foreground = Brushes.Black;
                 lblItemContentName.Background = Brushes.White;
+                lblItemContentName.Foreground = Brushes.Black;
                 lblItemContentExtensionTitle.Background = Brushes.White;
+                lblItemContentExtensionTitle.Foreground = Brushes.Black;
                 lblItemContentExtension.Background = Brushes.White;
+                lblItemContentExtension.Foreground = Brushes.Black;
                 lblItemContentCreatedTitle.Background = Brushes.White;
+                lblItemContentCreatedTitle.Foreground = Brushes.Black;
                 lblItemContentCreated.Background = Brushes.White;
+                lblItemContentCreated.Foreground = Brushes.Black;
                 lblItemContentLastModifiedTitle.Background = Brushes.White;
+                lblItemContentLastModifiedTitle.Foreground = Brushes.Black;
                 lblItemContentLastModified.Background = Brushes.White;
+                lblItemContentLastModified.Foreground = Brushes.Black;
             }
+            else
+            {
+                var DarkOne = new SolidColorBrush(Color.FromRgb(34, 34, 34));
+                var DarkTwo = new SolidColorBrush(Color.FromRgb(51, 51, 51)); ;
+                var DarkThree = new SolidColorBrush(Color.FromRgb(69, 69, 69));
+
+                foreach (var item in MenuButton.ItemContainerGenerator.Items)
+                {
+                    ((MenuItem)item).Background = DarkTwo;
+                    ((MenuItem)item).Foreground = Brushes.White;
+                }
+
+                MenuButton.Background = Brushes.Black;
+                MenuButton.Foreground = Brushes.White;
+
+                lblStatusLine.Foreground = Brushes.White;
+                MenuPanel.Background = DarkOne;
+                StatusLinePanel.Background = DarkOne;
+
+                TreeViewContainer.Background = DarkTwo;
+                TreeViewContainer.Foreground = Brushes.White;
+                ItemContentPanel.Background = DarkThree;
+
+                lblItemContentTitle.Background = DarkThree;
+                lblItemContentTitle.Foreground = Brushes.White;
+
+                lblItemContentName.Background = DarkThree;
+                lblItemContentName.Foreground = Brushes.White;
+
+                lblItemContentExtensionTitle.Background = DarkThree;
+                lblItemContentExtensionTitle.Foreground = Brushes.White;
+
+                lblItemContentExtension.Background = DarkThree;
+                lblItemContentExtension.Foreground = Brushes.White;
+
+                lblItemContentCreatedTitle.Background = DarkThree;
+                lblItemContentCreatedTitle.Foreground = Brushes.White;
+
+                lblItemContentCreated.Background = DarkThree;
+                lblItemContentCreated.Foreground = Brushes.White;
+
+                lblItemContentLastModifiedTitle.Background = DarkThree;
+                lblItemContentLastModifiedTitle.Foreground = Brushes.White;
+
+                lblItemContentLastModified.Background = DarkThree;
+                lblItemContentLastModified.Foreground = Brushes.White;
+            }
+        }
+        private void ViewDocking_Click(object sender, RoutedEventArgs e)
+        {
             ViewDocking ^= true;
+            ChangeAppearance();
+        }
+        private void ToggleDarkmode_Click(object sender, RoutedEventArgs e)
+        {
+            DarkThemeEnabled ^= true;
+            ChangeAppearance();
         }
         #endregion
     }
