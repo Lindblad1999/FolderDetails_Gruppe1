d48513d9b86dc40151517166e152e4b50baed087
DanielSimonsen90 <danielsimonsen90@gmail.com>
2020-09-21 10:11:28 +0200

    UI done
    Can become colorful and it's nice

--- FolderDetails/FolderDetails/MainWindow.xaml.cs
+++ FolderDetails/FolderDetails/MainWindow.xaml.cs
@@ -20,14 +20,52 @@ namespace FolderDetails
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool ViewDocking = false;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        #region MenuItems_Click
         private void OpenFolder_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void ViewDocking_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ViewDocking)
+            {
+                MenuPanel.Background = Brushes.Red;
+                StatusLinePanel.Background = Brushes.Chocolate;
+                TreeViewContainer.Background = Brushes.Orange;
+                ItemContentPanel.Background = Brushes.Yellow;
+                lblItemContentTitle.Background = Brushes.Lime;
+                lblItemContentName.Background = Brushes.Green;
+                lblItemContentExtensionTitle.Background = Brushes.DodgerBlue;
+                lblItemContentExtension.Background = Brushes.Blue;
+                lblItemContentCreatedTitle.Background = Brushes.Magenta;
+                lblItemContentCreated.Background = Brushes.DarkMagenta;
+                lblItemContentLastModifiedTitle.Background = Brushes.LightGray;
+                lblItemContentLastModified.Background = Brushes.Gray;
+            }
+            else
+            {
+                MenuPanel.Background = Brushes.White;
+                StatusLinePanel.Background = Brushes.White;
+                TreeViewContainer.Background = Brushes.White;
+                ItemContentPanel.Background = Brushes.White;
+                lblItemContentTitle.Background = Brushes.White;
+                lblItemContentName.Background = Brushes.White;
+                lblItemContentExtensionTitle.Background = Brushes.White;
+                lblItemContentExtension.Background = Brushes.White;
+                lblItemContentCreatedTitle.Background = Brushes.White;
+                lblItemContentCreated.Background = Brushes.White;
+                lblItemContentLastModifiedTitle.Background = Brushes.White;
+                lblItemContentLastModified.Background = Brushes.White;
+            }
+            ViewDocking ^= true;
+        }
+        #endregion
     }
 }
